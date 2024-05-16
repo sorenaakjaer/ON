adde306384534fd0387c9dc3731538d16d41728c
jsdevaccount <henrik@socialanalytics.dk>
2024-05-16 23:38:48 +0200

    Debugging

--- page_code_files/script.js
+++ page_code_files/script.js
@@ -3064,33 +3064,14 @@ $(document).one("trigger::vue_loaded", function () {
 					}
 				} else "true" == n && ($(".UM_EVENT_TYPE > input").val("ADD_GROUP"), $(".UM_USER_ID > input").val(e.id), $(".UM_GROUP_ID > input").val(t), $(".BTN_UserManagement > a").click()), "false" == n && ($(".UM_EVENT_TYPE > input").val("REMOVE_GROUP"), $(".UM_USER_ID > input").val(e.id), $(".UM_GROUP_ID > input").val(t), $(".BTN_UserManagement > a").click())
 			},
-			onActiveRoleNotificationsChange(theActiveUser, groupId, status) {
-				// Find the index of the active user in the users array
-				const userIndex = this.users.findIndex(user => user.id === this.theActiveUser.id);
+			onActiveRoleNotificationsChange(e, t, s) {
+				var a = this.users.findIndex(e => e.id == this.theActiveUser.id),
+					i = this.theActiveUser.role_array.findIndex(e => e.group_id_noti == t),
+					r = "false" == s ? "true" : "false";
+				this.theActiveUser.role_array[i].active_role_noti = r
 
-				// Find the index of the role in the active user's role array
-				const roleIndex = this.theActiveUser.role_array.findIndex(role => role.group_id_noti === groupId);
-
-				// Determine the new status based on the current status
-				const newStatus = status === "false" ? "true" : "false";
-
-				// Update the active role notification status for the active user
-				this.theActiveUser.role_array[roleIndex].active_role_noti = newStatus;
-
-				// Update the active role notification status in the users array
-				this.users[userIndex].role_array[roleIndex].active_role_noti = newStatus;
-
-				// Prepare the event type and set the necessary input values
-				const userId = theActiveUser.id;
-				const eventType = newStatus === "true" ? "ADD_GROUP" : "REMOVE_GROUP";
-				const groupIdToStr = groupId.toString();
-				console.log({ eventType, userId, groupIdToStr })
-				$(".UM_EVENT_TYPE > input").val(eventType);
-				$(".UM_USER_ID > input").val(userId);
-				$(".UM_GROUP_ID > input").val(groupIdToStr);
-
-				// Trigger the user management action
-				$(".BTN_UserManagement > a").click();
+				this.users[a].role_array[i].active_role_noti = r
+				"true" == r && ($(".UM_EVENT_TYPE > input").val("ADD_GROUP"), $(".UM_USER_ID > input").val(e.id), $(".UM_GROUP_ID > input").val(t), $(".BTN_UserManagement > a").click()), "false" == r && ($(".UM_EVENT_TYPE > input").val("REMOVE_GROUP"), $(".UM_USER_ID > input").val(e.id), $(".UM_GROUP_ID > input").val(t), $(".BTN_UserManagement > a").click())
 			},
 			setTheActiveUserRoleChangeModal(e) {
 				this.theActiveUser = e, this.isEditUserRoles = !0, this.isVueModalOverlay = !0, $("body").css("overflow", "")
